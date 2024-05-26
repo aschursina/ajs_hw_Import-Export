56d4b93bf1f94e9c4bba78b08553f8df15f56e79
Anastasia <n3636@mail.ru>
2024-05-26 21:58:10 +0300

    change config

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{var e={393:()=>{(new Game).start()}},r={};function t(o){var a=r[o];if(void 0!==a)return a.exports;var n=r[o]={exports:{}};return e[o](n,n.exports,t),n.exports}t.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return t.d(r,{a:r}),r},t.d=(e,r)=>{for(var o in r)t.o(r,o)&&!t.o(e,o)&&Object.defineProperty(e,o,{enumerable:!0,get:r[o]})},t.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),(()=>{"use strict";t(393)})()})();
+(()=>{"use strict";(new class{start(){console.log("game started")}}).start()})();
